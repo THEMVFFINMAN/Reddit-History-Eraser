58e7fc3ae5451dabde1f0615f2ed80f0467184d5
James Barnett <wes@wbarnett.us>
2017-04-04 09:37:16 -0500

    Update for Python 3
    - Allow keeping of more recent posts and comments
    - Use config.in for refresh token, instead of name/password

--- RedEraser.py
+++ RedEraser.py
@@ -1,136 +1,117 @@
-#!python2
 
 import praw
+import time
 
-# Enter your username and password below
-USERNAME  = "YOUR USERNAME"
-PASSWORD  = "YOUR PASSWORD"
+ignoreSubs = {}
+daysToKeep = 0
 
-# Enter list of subreddits to ignore in lowercase
-IGNORE_SUBREDDITS = {}
+timeToKeep = daysToKeep * 60 * 60 * 24
 
-# Leave this as -1 if you want to erase everything
-# Otherwise change to how many comments or posts you want deleted
-commentsToDelete = -1
-postsToDelete = -1
-
-'''--------------------------------------------------------'''
-#                                                            #
-# Generally the things below this line should not be touched #
-#                                                            #
-'''--------------------------------------------------------'''
-
-# Leave the useragent as is
-USERAGENT = "Comment Deleter"
-
-# This just tracks how many comments and posts have been deleted so far
-# Do not touch
-commentsDeleted = 0
-postsDeleted = 0
-
-# This simply logs you in and returns a user object
 def login():
-    r = praw.Reddit(USERAGENT)
-    r.login(USERNAME, PASSWORD)
-    user = r.get_redditor(USERNAME)
+    f = open("RedEraser-Token", "r")
+    my_client_id = f.readline().strip('\n') 
+    my_client_secret = f.readline().strip('\n')
+    my_refresh_token = f.readline().strip('\n')
+    f.close()
+    r = praw.Reddit(user_agent="Red Eraser / v 2.0", 
+            client_id=my_client_id,
+            client_secret=my_client_secret,
+            refresh_token=my_refresh_token)
+    print(r.user.me())
+    return r.user.me()
 
-    print "Successfully logged %s in" % USERNAME
+def deleteComments(user):
 
-    return user
+    cscore_min = 200
+    cscore_tot = 0
+    currenttime = time.time()
+    print("Deleting comments now")
 
-# Takes the user object and deletes the users comments up to the amount
-# specified in commentsToDelete above
-def commentDeleter(user, commentsToDelete):
+    commentsDeleted = 0
 
-    print "Deleting comments now"
+    cgen = user.comments.new(limit=None)
 
-    # Tracks how many comments have deleted so far
-    global commentsDeleted
+    for c in cgen:
 
-    # By accessing the 100 comment limit one billion times,
-    # it's safe to assume that this will delete any and all comments/posts (up to 100 billion)
-    for o in xrange(0,999999999):
+        if str(c.subreddit).lower() in ignoreSubs:
+            print("Ignoring")
+            continue
 
-        # This is a simple check after one run through to see if there were any comments to delete
-        if o > 0 and commentsDeleted == 0:
-            print "No comments to delete!"
-            return
-
-        # Due to the limits on reddit's API, this for loop can only access 100 comments at a time
-        for c in user.get_comments(limit=None):
-
-            # Because reddit only saves the most recently edited comment,
-            # each comment is edited with a pound symbol and then deleted
-            if str(c.subreddit).lower() in IGNORE_SUBREDDITS:
-                continue
+        if (currenttime - c.created_utc > timeToKeep):
             c.edit('#')
             c.delete()
-
             commentsDeleted += 1
-            print "Comment Deleted"
+            print("Comment deleted")
 
-            # It checks if it's deleted your specified amount of comments,
-            # if left at '-1' this condition will never be met and it will delete all comments
-            if commentsDeleted == commentsToDelete:
-                print "{0} Comments Deleted".format(commentsDeleted)
-                return
-
-            # After every 10 comments it prints how many comments have been deleted so far
-            if commentsDeleted % 10 == 0:
-                print "{0} Comments Deleted".format(commentsDeleted)
+        if commentsDeleted % 10 == 0:
+            print("{0} comments deleted".format(commentsDeleted))
 
     if commentsDeleted != 0:
-        print "All {0} Comments Deleted".format(postsDeleted)
+        print("Total of {0} comments deleted".format(commentsDeleted))
 
-# This is almost identical to commentDeleter()
-def postDeleter(user, postsToDelete):
+    return commentsDeleted
 
-    print "Deleting posts now"
+def deletePosts(user):
 
-    global postsDeleted
+    currenttime = time.time()
+    print("Deleting posts now")
+    postsDeleted = 0
+    postsFound = 0
 
-    for o in xrange(0,999999999):
+    for o in range(0,999999999):
 
-        if o > 0 and postsDeleted == 0:
-            print "No posts to delete!"
-            return
+        postFound = 0
 
-        for p in user.get_submitted(limit=None):
-            if str(p.subreddit).lower() in IGNORE_SUBREDDITS:
+        for p in user.submissions.new(limit=100):
+
+            postsFound += 1
+
+            if str(p.subreddit).lower() in ignoreSubs:
+                print("Ignoring")
                 continue
-            if p.selftext:
-                p.edit('#')
-            p.delete()
-            postsDeleted += 1
 
-            if postsDeleted == postsToDelete:
-                print "{0} Posts Deleted".format(postsDeleted)
-                return
+            if (currenttime - p.created_utc > timeToKeep):
+                if p.selftext:
+                    p.edit('#')
+                p.delete()
+                postsDeleted += 1
 
             if postsDeleted % 10 == 0:
-                print "{0} Posts Deleted".format(postsDeleted)
+                print("{0} posts deleted".format(postsDeleted))
+
+        if postsFound < 100:
+            print("Done.")
+            break
 
     if postsDeleted != 0:
-        print "All {0} Posts Deleted".format(postsDeleted)
+        print("Total of {0} posts deleted".format(postsDeleted))
+
+    return postsDeleted
 
 def main():
     try:
         user = login()
-    except Exception, e:
-        print "Login failed due to: "
-        print str(e)
+    except Exception as e:
+        print("Login failed due to: ")
+        print(str(e))
         return
 
     try:
-        commentDeleter(user, commentsToDelete)
-        postDeleter(user, postsToDelete)
-    except Exception, e:
-        print "Deletion failed due to: "
-        print str(e)
+        commentsDeleted = deleteComments(user)
+    except Exception as e:
+        print("Comment deletion failed due to: ")
+        print(str(e))
         return
 
-    print "All {0} Comments and {1} posts Deleted".format(commentsDeleted, postsDeleted)
+    try:
+        postsDeleted = deletePosts(user)
+    except Exception as e:
+        print("Post deletion failed due to: ")
+        print(str(e))
+        return
 
+    print("Total of {0} comments and {1} posts deleted".format(commentsDeleted,
+        postsDeleted))
 
 if __name__ == "__main__":
     main()
